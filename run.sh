31e138cfd428cc2621ac855e22cf41ba0d1c3453
Ben Liu <arkilis@gmail.com>
2026-03-21 07:28:42 +1000

    更新 NAS 上面的具体路径

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
-/Users/ben/Developer/other_dev/synology-photo-ai-tags/bin/python -m src \
+export PATH=/usr/local/bin:$PATH
+
+/volume1/homes/ben/scripts/synology-photo-ai-tags/.synology-photo-ai-tags/bin/python -m src \
   --model gemini-2.5-flash \
   --requests-per-minute 8 \
   --max-files-per-run 220 \
-  --progress /Volumes/homes/ben/Photos/.ai-tags-progress.json \
-  --root /Volumes/homes/ben/Photos/
+  --progress /var/services/homes/ben/Photos/.ai-tags-progress.json \
+  --root ~/Photos
   # --root /Volumes/homes/ben/Photos/MobileBackup/iPhone/2026/test
